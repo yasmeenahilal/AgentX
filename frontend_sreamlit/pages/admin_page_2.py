98c416fdbea564aba511f2ec5f16955268e7c8d1
w7Ali <mr.wahidali7c@gmail.com>
2025-04-14 22:05:25 +0530

    Removed Hugging Face access tokens from source files

---
+++ frontend_sreamlit/pages/admin_page_2.py
@@ -0,0 +1,35 @@
+import requests
+import streamlit as st
+
+st.title("Admin Page 2 - Agent Management")
+
+# Example API usage: Create Agent
+st.header("Create Agent")
+agent_name = st.text_input("Agent Name", value="MyBot")
+user_id = st.text_input("User ID", value="user1")
+index_name = st.text_input("Index Name", value="thursday")
+llm_provider = st.text_input("LLM Provider", value="huggingface")
+llm_model_name = st.text_input(
+    "LLM Model Name", value="mistralai/Mixtral-8x7B-Instruct-v0.1"
+)
+llm_api_key = st.text_input(
+    "LLM API Key", value="pass"
+)
+prompt_template = st.text_input(
+    "Propmt",
+    value="You are a knowledgeable assistant trained to provide answers based on accurate, reliable, and factual information. If you do not have enough data to answer a question, do not invent information or provide a speculative response. Instead, acknowledge the lack of sufficient data and refrain from answering. Only answer questions that you can confirm with the provided data. If the query is ambiguous or outside the scope of the data, state 'I do not have enough information to answer that.",
+)
+
+
+if st.button("Create Agent"):
+    payload = {
+        "agent_name": agent_name,
+        "user_id": user_id,
+        "index_name": index_name,
+        "llm_provider": llm_provider,
+        "llm_model_name": llm_model_name,
+        "llm_api_key": llm_api_key,
+        "prompt_template": prompt_template,
+    }
+    response = requests.post("http://0.0.0.0:5016/agent/create_agent", json=payload)
+    st.json(response.json())
---
+++ schemas/agent_schemas.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class GetAgentRequest(BaseModel):
+    agent_name: str
+    user_id: str
+
+
+class CreateAgentRequest(BaseModel):
+    agent_name: str = "MyBot"
+    user_id: str = "user1"
+    index_name: str = "index1"
+    llm_provider: str = "huggingface"  # e.g., "huggingface", "openai", "gemini"
+    llm_model_name: str = (
+        "mistralai/Mistral-7B-Instruct-v0.3"  # "mistralai/Mixtral-8x7B-Instruct-v0.1"
+    )
+    llm_api_key: str
+    prompt_template: str = (
+        "You are a knowledgeable assistant trained to provide answers based on accurate, reliable, and factual information. If you do not have enough data to answer a question, do not invent information or provide a speculative response. Instead, acknowledge the lack of sufficient data and refrain from answering. Only answer questions that you can confirm with the provided data. If the query is ambiguous or outside the scope of the data, state 'I do not have enough information to answer that."
+    )
+
+
+class UpdateAgentRequest(BaseModel):
+    agent_name: str
+    user_id: str
+    index_name: str
+    llm_provider: Optional[str] = None
+    llm_model_name: Optional[str] = None
+    llm_api_key: Optional[str] = None
+    prompt_template: Optional[str] = None
+
+
+class QuerAgentRequest(BaseModel):
+    agent_name: str = "MyBot"
+    user_id: str = "user1"
+    question: str = "What is the name of the candidate"
+
+
+class DeleteAgent(BaseModel):
+    agent_name: str
+    user_id: str
+    index_name: str
